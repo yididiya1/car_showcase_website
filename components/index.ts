0d12de959f6ff025718a6d1164d1b84a01c790d1
yididiya1 <39265570+yididiya1@users.noreply.github.com>
2024-04-13 21:20:53 +0300

    Layout (#4)
    * Hero page section implemented
    
    * Site layout including footer and header implemented
    
    * Hero page section implemented

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,12 @@
 import Hero from "./Hero";
 import CustomBotton from './CustomBotton';
 
+import Navbar from './Navbar';
+import Footer from './Footer';
+
 export{
     Hero,
-    CustomBotton
+    CustomBotton,
+    Navbar,
+    Footer
 }
